55a6a364ff4e7a1a5c3c6d8e0d56a8f237a5c073
Nikul30701 <p.nikul6403@gmail.com>
2024-12-18 23:34:01 +0530

    App Completed

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("", views.signup, name="signup"),
     path("login/", views.loginn, name="login"),
     path("todopage/", views.todo, name="todo"),
+    path('edit_todo/<int:srno>', views.edit_todo, name='edit_todo'),
+    path('delete_todo/<int:srno>', views.delete_todo),
+    path("signout/",views.signout, name='signout')
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import authenticate, login, logout
-
+from django.contrib.auth.decorators import login_required
 from todo import models
 
 
@@ -38,7 +38,7 @@ def loginn(request):
 
     return render(request, "login.html")
 
-
+@login_required(login_url='/login')
 def todo(request):
     if request.method == "POST":
         title = request.POST.get('title')
@@ -53,3 +53,31 @@ def todo(request):
 
     res = models.TODOO.objects.filter(user=request.user).order_by('-date')
     return render(request, "todo.html", {'res': res})
+
+
+@login_required(login_url='/login')
+def edit_todo(request, srno):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        print(title)  # Debugging print statement
+
+        obj = models.TODOO.objects.get(srno=srno)
+        obj.title=title
+        obj.save()
+        user = request.user
+
+        # Correct ordering field
+        return redirect('/todopage' )
+
+    obj = models.TODOO.objects.get(srno=srno)
+    return render(request, "edit_todo.html",{'obj':obj})
+
+@login_required(login_url='/login')
+def delete_todo(request, srno):
+    obj = models.TODOO.objects.get(srno=srno)
+    obj.delete()
+    return redirect('/todopage')
+
+def signout(request):
+    logout(request)
+    return redirect('/login')
